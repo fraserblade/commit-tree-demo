6c559b6bae34397814a5e3bf9594b37c86b6545b
Chris Scudds <chris.scudds@gmail.com>
2023-10-16 16:58:34 +0100

    refactor and unit tests

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -1,12 +1,15 @@
+package src.main.java;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-class Employee {
+public class Employee {
     private final String name;
     private final List<Employee> reports;
     private final int empNo;
     private int commitCount;
+    private String employeeID;
     static Random random = new Random();
     static private int empCounter = 1;
 
@@ -23,6 +26,7 @@ class Employee {
         this.reports = new ArrayList<>();
         this.commitCount = commits;
         this.empNo = empCounter++;
+        this.employeeID = EmployeeIDGenerator.generateUniqueEmployeeID();
     }
 
     public void addReport(Employee employee) {
@@ -41,8 +45,9 @@ class Employee {
         return commitCount;
     }
 
+    public String getEmployeeID() { return employeeID; }
     @Override
     public String toString() {
-        return this.getName() + " id " + this.empNo + (reports.size() > 0 ? " " + reports.size() : "");
+        return this.getName() + " - " + this.employeeID + (reports.size() > 0 ? " (" + reports.size() + ")" : "");
     }
 }
